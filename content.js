47b20c48132e689377084631e6832372305f4634
Reland Boyle <reland.boyle@gmail.com>
2021-10-15 01:20:36 -0400

    211015 Before Update

---
+++ content.js
@@ -0,0 +1,31 @@
+console.log('RBW ZD Agent Helper is Active!');
+
+const agentHelper = () => {
+    console.log('RBW ZD Agent Helper is Running!');
+    
+    const createButton = document.getElementsByClassName("ember-view btn organization-pill create");
+    
+    const orgButton = document.getElementsByClassName("ember-view btn organization-pill");
+
+    for (const element of createButton) {
+        if (element.innerText.includes('(create)')) {
+            element.style['background-color'] = "#BD322C";
+            element.style['color'] = '#FFFFFF';
+            element.style['font-weight'] = 'bold';
+        } else {
+            element.style['background-color'] = "#F8F9F9";
+            element.style['color'] = '#2F3941';
+            element.style['font-weight'] = 'normal';
+        }
+    }
+
+    for (const element of orgButton) {
+        if (!element.innerText.includes('(create)')) {
+            element.style['background-color'] = "#F8F9F9";
+            element.style['color'] = '#2F3941';
+            element.style['font-weight'] = 'normal';
+        }
+    }
+}
+
+const timedExecution = setInterval(agentHelper, 1000);
