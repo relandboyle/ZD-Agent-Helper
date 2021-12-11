2376563f024254fb1c35ead8a6e25ee775ad0b71
relandboyle <reland.boyle@gmail.com>
2021-12-10 21:51:15 -0500

    Top Bar Update

--- content.js
+++ content.js
@@ -1,31 +1,35 @@
 console.log('RBW ZD Agent Helper is Active!');
 
 const agentHelper = () => {
-    console.log('RBW ZD Agent Helper is Running!');
-    
-    const createButton = document.getElementsByClassName("ember-view btn organization-pill create");
-    
-    const orgButton = document.getElementsByClassName("ember-view btn organization-pill");
+  console.log('RBW ZD Agent Helper is Running!');
 
-    for (const element of createButton) {
-        if (element.innerText.includes('(create)')) {
-            element.style['background-color'] = "#BD322C";
-            element.style['color'] = '#FFFFFF';
-            element.style['font-weight'] = 'bold';
-        } else {
-            element.style['background-color'] = "#F8F9F9";
-            element.style['color'] = '#2F3941';
-            element.style['font-weight'] = 'normal';
-        }
-    }
+  const createButtons = document.querySelectorAll('.organization-pill.create');
+  const orgButtons = document.querySelectorAll('.organization-pill');
 
-    for (const element of orgButton) {
-        if (!element.innerText.includes('(create)')) {
-            element.style['background-color'] = "#F8F9F9";
-            element.style['color'] = '#2F3941';
-            element.style['font-weight'] = 'normal';
-        }
+  createButtons.forEach((element) => {
+    const topBar = element.closest('header');
+    if (element.innerText.includes('(create)')) {
+      topBar.style.backgroundColor = '#BD322C';
+      element.style.backgroundColor = '#BD322C';
+      element.style.color = '#FFFFFF';
+      element.style.fontWeight = 'bold';
+    } else {
+      topBar.style.backgroundColor = '#F8F9F9';
+      element.style.backgroundColor = '#F8F9F9';
+      element.style.color = '#2F3941';
+      element.style.fontWeight = 'normal';
     }
+  });
+
+  orgButtons.forEach((element) => {
+    const topBar = element.closest('header');
+    if (!element.innerText.includes('(create)')) {
+      topBar.style.backgroundColor = '#F8F9F9';
+      element.style.backgroundColor = '#F8F9F9';
+      element.style.color = '#2F3941';
+      element.style.fontWeight = 'normal';
+    }
+  });
 }
 
 const timedExecution = setInterval(agentHelper, 1000);
